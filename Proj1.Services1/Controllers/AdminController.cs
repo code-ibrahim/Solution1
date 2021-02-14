c9dadf0bba7dbc1b209c4402083430bfdbf64eab
Mohd Ibrahim Sheikh <mohdibrahimsheikh15@gmail.com>
2021-02-14 17:34:42 +0530

    Ibrahim: initial commit

---
+++ Proj1.Services1/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proj1.Services1.Controllers
+{
+    //[Route("[controller]")]
+    //[Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AdminController : Controller
+    {
+        public AdminController()
+        {
+
+        }
+
+        [HttpGet]
+        public JsonResult Getmethod1()
+        {
+            return Json("testing 1st get method of AdminController");
+        }
+    }
+}
